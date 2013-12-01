7d2ba7498cfea20bf3ecca7539eb63e7dd8f18ba
Pablo Roman <pablo.roman85@gmail.com>
2013-12-01 16:11:36 +0000

    Hoempage, some design, models layout

--- app/models.py
+++ app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.db.models.fields import PositiveIntegerField
+
+import hashlib
+
+class PositiveBigIntegerField(PositiveIntegerField):
+    """Represents MySQL's unsigned BIGINT data type (works with MySQL only!)"""
+    empty_strings_allowed = False
+
+    def get_internal_type(self):
+        return "PositiveBigIntegerField"
+
+    def db_type(self, connection):
+        # This is how MySQL defines 64 bit unsigned integer data types
+        return "bigint UNSIGNED"
+
 
 # Create your models here.
 class Band(models.Model):
+    id = PositiveBigIntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -10,20 +26,31 @@ class Band(models.Model):
     yearsactive = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
     themes = models.CharField(max_length=200)
+    
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.name
 
+
 class Album(models.Model):
+    id = PositiveBigIntegerField(primary_key=True)
     band = models.ForeignKey(Band)
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
     label = models.CharField(max_length=200)
     cover = models.CharField(max_length=200)
+    
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.name
+        
+    def cached_cover(self):
+        hash = hashlib.sha1(str(self.id)).hexdigest()
+        return '/static/images/'+hash[0]+'/'+hash[1]+'/'+hash+'.jpg'
+
 
 class Review(models.Model):
+    id = PositiveBigIntegerField(primary_key=True)
     band = models.ForeignKey (Band)
     album = models.ForeignKey (Album)
-    pub_date = models.DateTimeField('Published on')
+    date = models.DateTimeField('Published on')
     score = models.IntegerField(default=0)
--- app/views.py
+++ app/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, get_object_or_404
-
-from app.models import Band
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count, Min, Sum, Avg
+from app.models import Album, Band, Review
 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
-    
+
+# SELECT `app_album`.*, AVG(`score`) as `avg` FROM `app_album` INNER JOIN `app_review` ON `app_album`.`id` = `app_review`.`album_id` WHERE `app_review`.`date` > "2013-10-01" AND `app_album`.`year` = "2013" GROUP BY `album_id` ORDER BY `avg`
+
+    album_list = Album.objects.filter(review__date__gte='2013-10-01').filter(year__gte='2013').annotate(average_score=Avg('review__score')).order_by('-average_score')[:24]
+    return render(request, 'home.html', { 'albums': album_list })
+
+def band_search(request):
+    return render(request, 'bands/search.html')
+
 def band_index(request):
-    band_list = Band.objects.order_by('name')[:5]
-    context = { 'bands': band_list }
-    return render(request, 'bands/index.html', context)
-	
+    band_list = Band.objects.order_by('name')
+    paginator = Paginator(band_list, 20)
+    page = request.GET.get('page')
+    try:
+    	band_list = paginator.page(page)
+    except PageNotAnInteger:
+    	band_list = paginator.page(1)
+    except EmptyPage:
+    	band_list = paginator.page(paginator.num_pages)
+    	
+    return render(request, 'bands/index.html', { 'bands': band_list })
+
 def band_show(request, band_id):
 	band = get_object_or_404(Band, pk=band_id)
 	return render(request, 'bands/show.html', { 'band': band })
+	
+def album_index(request):
+    return render(request, 'albums/index.html')
+
+def album_show(request, album_id):
+    return render(request, 'albums/show.html')
+
+def genre_index(request):
+    return render(request, 'genres/index.html')
+
+def genre_show(request, genre_id):
+    return render(request, 'genres/show.html')
+
+
